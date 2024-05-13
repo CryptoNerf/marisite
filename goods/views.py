7371f3b32d8aeb4e6c049051ac1bd4a3775d0062
cryptonerf <maesai1337@gmail.com>
2024-05-13 21:03:16 +0400

    After adding fixtures for goods

--- goods/views.py
+++ goods/views.py
@@ -1,9 +1,39 @@
 from django.shortcuts import render
+from django.template import context
 
 
 def catalog(request):
-    return render(request, 'goods/catalog.html')
+    context = {
+        "title": "Украшения Мари - Каталог",
+        "goods": [
+            {
+                "image": "deps/image/грибочкисерьга.png",
+                "name": "pagegood1"
+            },
+            {
+                "image": "deps/image/грибочкисерьга.png",
+                "name": "Чайный столик и два стула"
+            },
+            {
+                "image": "deps/image/грибочкисерьга.png",
+                "name": "Двухспальная кровать"
+            },
+            {
+                "image": "deps/image/грибочкисерьга.png",
+                "name": "Кухонный стол с раковиной"
+            },
+            {
+                "image": "deps/image/грибочкисерьга.png",
+                "name": "Кухонный стол с встройкой"
+            },
+            {
+                "image": "deps/image/грибочкисерьга.png",
+                "name": "Угловой диван для гостинной"
+            },
+        ],
+    }
+    return render(request, "goods/catalog.html", context)
 
 
 def product(request):
-    return render(request, 'goods/product.html')
+    return render(request, "goods/product.html")
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.template import context
+
+
 
 
 def buypage(request):
